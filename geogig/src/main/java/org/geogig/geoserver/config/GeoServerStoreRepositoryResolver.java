6e314065cf9652ba8c268b4ab3bb8be6c0b113c4
Erik Merkle <emerkle@boundlessgeo.com>
2015-11-30 09:32:32 -0600

    updating File to URI code to match geogig-fix-master branch

--- geogig/src/main/java/org/geogig/geoserver/config/GeoServerStoreRepositoryResolver.java
+++ geogig/src/main/java/org/geogig/geoserver/config/GeoServerStoreRepositoryResolver.java
@@ -3,23 +3,20 @@ package org.geogig.geoserver.config;
 import java.io.IOException;
 import java.net.URI;
 
-import org.apache.commons.io.FilenameUtils;
 import org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory;
 
 import com.google.common.base.Throwables;
 import java.io.File;
 
 public class GeoServerStoreRepositoryResolver implements GeoGigDataStoreFactory.RepositoryLookup {
-    
+
     @Override
     public URI resolve(final String repository) {
         RepositoryManager repositoryManager = RepositoryManager.get();
         try {
             RepositoryInfo info = repositoryManager.get(repository);
-            //enforce unix file seperators
-            File unixsep = new File(info.getLocation());
-            //check if refers to file, if it does add the file prefix
-            return unixsep.toURI();
+            File locFile = new File(info.getLocation());
+            return locFile.toURI();
         } catch (IOException e) {
             throw Throwables.propagate(e);
         }
